d3e889651ebed63ee31361e8e934657d8c4104ff
Autierus <104576663+Autierus@users.noreply.github.com>
2024-08-23 11:29:21 -0300

    Add files via upload

---
+++ Lista1/Exercicio4.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        //para digitar o código do produto
+        int codigo = Integer.parseInt( JOptionPane.showInputDialog("Digite o código do produto"));
+        if ((codigo <= 10) && (codigo > 0)) {
+           //para digitar a quantidade comprada
+        int quant = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de produtos")) 
+        }
+
+        
+    }
+    
+}
